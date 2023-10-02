df7edea5ea34201fdfd2c68bcea95f9e5e240c26
ankush.gupta <ankush.gupta@msbdocs.com>
2023-10-02 20:00:47 +0530

    ip debug

--- main.js
+++ main.js
@@ -11,11 +11,12 @@ const port = parseInt(process.env.port);
 
 // Initialize an object aka self expirinng hash map to store request api count
 const routeRequestCounts = new TimedMap(rateLimitWindowSec * 60 * 1000);
+app.set('trust proxy', true);
 
 // this is the middle thing aka middleware to have the request count for different ip's
 app.use((req, res, next) => {
   const ip =
-    req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   if (!routeRequestCounts.get(ip)) {
     routeRequestCounts.set(ip, 1); // Initialize count if not already present
   } else {
@@ -39,7 +40,8 @@ app.use((req, res, next) => {
 // api 1
 app.get("/api1", async (req, res) => {
   const ip =
-    req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+     req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    console.log(ip)
   res.status(200).json({
     calls: routeRequestCounts.get(ip),
     message: "this is your api1",

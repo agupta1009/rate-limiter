479e69542d730b7cce5676d99faffb11fce35c53
ankush.gupta <ankush.gupta@msbdocs.com>
2023-10-02 19:25:31 +0530

    formatted code

--- TimedMap.js
+++ TimedMap.js
@@ -1,44 +1,44 @@
 // self expiring hash map
 
 class TimedMap {
-    constructor(expirationTimeMs) {
-      this.data = {};
-      this.expirationTimeMs = expirationTimeMs;
-    }
-  
-    // set key with expiration
-    set(key, value) {
-      console.log(Date.now())
-      if (!this.data[key] || Date.now() >= this.data[key].expirationTime) {
-        this.data[key] = {
-          value,
-          expirationTime: Date.now() + this.expirationTimeMs,
-        };
-  
-        // Set a timeout to remove the entry when it expires
-        setTimeout(() => {
-          this.remove(key);
-        }, this.expirationTimeMs);
-      }else {
-        // Update the value while keeping the previous expiration time
-        this.data[key].value = value;
-      }
-    }
-  
-    // Get key if exists or not expired
-    get(key) {
-      const entry = this.data[key];
-      if (entry && Date.now() < entry.expirationTime) {
-        return entry.value;
-      }
-      return undefined;
-    }
+  constructor(expirationTimeMs) {
+    this.data = {};
+    this.expirationTimeMs = expirationTimeMs;
+  }
 
-// remove key
+  // set key with expiration
+  set(key, value) {
+    console.log(Date.now());
+    if (!this.data[key] || Date.now() >= this.data[key].expirationTime) {
+      this.data[key] = {
+        value,
+        expirationTime: Date.now() + this.expirationTimeMs,
+      };
+
+      // Set a timeout to remove the entry when it expires
+      setTimeout(() => {
+        this.remove(key);
+      }, this.expirationTimeMs);
+    } else {
+      // Update the value while keeping the previous expiration time
+      this.data[key].value = value;
+    }
+  }
+
+  // Get key if exists or not expired
+  get(key) {
+    const entry = this.data[key];
+    if (entry && Date.now() < entry.expirationTime) {
+      return entry.value;
+    }
+    return undefined;
+  }
+
+  // remove key
   remove(key) {
-    console.log(Date.now())
+    console.log(Date.now());
     delete this.data[key];
   }
 }
 
-module.exports=TimedMap
+module.exports = TimedMap;
--- main.js
+++ main.js
@@ -1,61 +1,64 @@
-const express = require("express")
-const app = express()
-const TimedMap = require('./TimedMap');
-const dotenv = require('dotenv');
+const express = require("express");
+const app = express();
+const TimedMap = require("./TimedMap");
+const dotenv = require("dotenv");
 dotenv.config();
 
-
-//  Load the config file 
-const rateLimitWindowSec = parseInt(process.env.RATE_LIMIT_WINDOW_SEC) || 1; 
+//  Load the config file
+const rateLimitWindowSec = parseInt(process.env.RATE_LIMIT_WINDOW_SEC) || 1;
 const maxRequests = parseInt(process.env.RATE_LIMIT_MAX_REQUESTS) || 100;
-const port = parseInt(process.env.port)
+const port = parseInt(process.env.port);
 
 // Initialize an object aka self expirinng hash map to store request api count
 const routeRequestCounts = new TimedMap(rateLimitWindowSec * 60 * 1000);
 
 // this is the middle thing aka middleware to have the request count for different ip's
 app.use((req, res, next) => {
-    const ip = req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress;
+  const ip =
+    req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   if (!routeRequestCounts.get(ip)) {
-    routeRequestCounts.set(ip,1); // Initialize count if not already present
-  }else{
+    routeRequestCounts.set(ip, 1); // Initialize count if not already present
+  } else {
     routeRequestCounts.set(ip, 1 + routeRequestCounts.get(ip));
   }
-  if(routeRequestCounts.get(ip)> maxRequests){
-    console.log("number of allowed call exceeded")
+  if (routeRequestCounts.get(ip) > maxRequests) {
+    console.log("number of allowed call exceeded");
     return res.status(429).json({
-        message:"number of allowed api calls exceeded. Allowed calls: " + maxRequests + " Current Calls: " + routeRequestCounts.get(ip)
-    })
-
+      message:
+        "number of allowed api calls exceeded. Allowed calls: " +
+        maxRequests +
+        " Current Calls: " +
+        routeRequestCounts.get(ip),
+    });
   }
   next();
 });
 
-// api routes 
+// api routes
 
 // api 1
-app.get('/api1', async (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress
-    console.log(ip)
-    res.status(200).json({
-        "calls":routeRequestCounts.get(ip),
-        "message":"this is your api1"
-    })
-  });
-
-
-// api 2
-  app.get('/api2', async (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress
-  console.log(ip)
+app.get("/api1", async (req, res) => {
+  const ip =
+    req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log(ip);
   res.status(200).json({
-      "calls":routeRequestCounts.get(ip),
-      "message":"this is your api2"
-  })
+    calls: routeRequestCounts.get(ip),
+    message: "this is your api1",
+  });
 });
 
+// api 2
+app.get("/api2", async (req, res) => {
+  const ip =
+    req.ip || req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log(ip);
+  res.status(200).json({
+    calls: routeRequestCounts.get(ip),
+    message: "this is your api2",
+  });
+});
 
 // server port
-app.listen(port,()=>{
-    console.log("server started at port: "+ port);
-})
+app.listen(port, () => {
+  console.log("server started at port: " + port);
+});

9cf12010026b97c597968f873fb0d7aa36814c04
Ankush Gupta <67683002+agupta1009@users.noreply.github.com>
2023-10-02 14:04:20 +0530

    Rate limit on api for a user (Ip specific}
    a map is used that is acting as a cache to store the count of api w.r.t user ip. (can use redis in prod)

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
 const express=require("express")
+const path=require("path")
 const app=express()
 
 
+
+
+// Initialize an object to store request counts for different routes
+const routeRequestCounts = {};
+
+// Da middle thing to have the request count for different ips
+app.use((req, res, next) => {
+    const ip=req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress
+  if (!routeRequestCounts[ip]) {
+    routeRequestCounts[ip] = 1; // Initialize count if not already present
+  }else{
+    routeRequestCounts[ip]++;
+  }
+  if(routeRequestCounts[ip]>20){
+    console.log("number of allowed call exceeded")
+    return res.status(429).json({
+        message:"number of allowed api calls exceeded. \n Allowed calls: 20 \n Current Calls: " + routeRequestCounts[ip]
+    })
+
+  }
+  next();
+});
+
 // api routes 
-app.get('/api1', (req, res) => {
+app.get('/api1', async (req, res) => {
+    const ip=req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress
+    console.log(ip)
     res.status(200).json({
-        "success":"true",
-        "message":"this is your api 1"
+        "calls":routeRequestCounts[ip],
+        "message":"this is your api1"
     })
   });
+
+  app.get('/api2', async (req, res) => {
+  const ip=req.headers['x-forwarded-for'] || req.ip || req.connection.remoteAddress
+  console.log(ip)
+  res.status(200).json({
+      "calls":routeRequestCounts[ip],
+      "message":"this is your api2"
+  })
+});
 
 
 

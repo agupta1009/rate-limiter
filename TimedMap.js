ce2bf5478f818c4560529bfaa93856868b093f23
ankush.gupta <ankush.gupta@msbdocs.com>
2023-10-03 09:22:10 +0530

    TODO add a timeout to remove the network calls after its finished its processing

--- TimedMap.js
+++ TimedMap.js
@@ -22,6 +22,9 @@ class TimedMap {
       // Update the value while keeping the previous expiration time
       this.data[key].value = value;
     }
+
+    // TODO: add a set timeout to remove the count of network calls ie after every 2 sec for example, a network call aka api has finished its processing
+    // => this.data[key].value=this.data[key].value -1;
   }
 
   // Get key if exists or not expired
